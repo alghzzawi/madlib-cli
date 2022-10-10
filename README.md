6fc7ced418931785a2f5dfaad7685439ab4f2a83
alghzzawi <mohammadalghazzawi9@gmail.com>
2022-10-11 00:01:51 +0300

    game is Done

---
+++ README.md
@@ -0,0 +1,18 @@
+# LAB - Class 04
+
+## Project: Madlib-cli
+
+## Author: Mohammad alghzawi
+
+## Links and Resources
+* ### None
+
+## Setup
+* ### python 3.9.5
+* ### pytest 7.1.3
+
+## How to initialize/run your application (where applicable)
+
+* ### python madlib_cli/madlib.py
+* ### pytest tests/test_madlib.py   
+
---
+++ madlib_cli/madlib.py
@@ -0,0 +1,45 @@
+import re
+
+def read_template(path):
+    try:
+        with open(path, 'r') as f:
+            content = f.read()
+    except FileExistsError as err:
+        return err
+    except FileNotFoundError as err:
+        return err
+    return content 
+
+def parse_template(story):
+    array_of_parts= re.findall(r"(?<=\{).+?(?=\})",story)
+    newText=story
+    for word in array_of_parts:
+        newText = str(newText.replace(word,''))
+    return newText,tuple(array_of_parts)
+
+def merge(newText,parts):
+    mergeStory = newText.format(*parts)
+    return mergeStory
+
+if __name__ == '__main__':
+    print('''
+*************************************************************************
+**  Welcome to madlibs game, Mad Lib is a game that replacement game.  **
+**                                                                     **
+**  rules this game:                                                   **
+**  you will enter some words and then it will replaced in a paragraph,**
+**  it may be a funny, just anser the questions and press enter.       **
+*************************************************************************''')
+    path = "assets/make_me_a_video_game_template.txt"
+    story=read_template(path)
+    newText,array_of_parts=parse_template(story)
+    parts=[]
+    for part in array_of_parts:
+        parts.append(input('Enter {} ==>  '.format(part)))
+    # parts=("dark", "stormy", "night")
+    newStory= merge(newText,parts)
+    
+    with open('assets/response.txt','wt') as new_funny_Story:
+        new_funny_Story.write(newStory)
+    with open('assets/response.txt') as read_text:
+        print(read_text.read())
